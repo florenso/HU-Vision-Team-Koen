c66ccfa9688b03c20608a3858da28e73d34a46dd
koen de gruijter <koen.degruijter@student.hu.nl>
2016-04-15 17:25:04 +0200

    made stepToIntensityImage

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,8 +1,16 @@
 #include "StudentPreProcessing.h"
-
+#include "IntensityImageStudent.h"
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	return nullptr;
+	IntensityImageStudent * iImg = new IntensityImageStudent(image.getWidth(), image.getHeight());
+	const int size = image.getWidth()* image.getHeight();
+		for(int i=0; i < size; i++){
+			RGB pixel = image.getPixel(i);
+			Intensity grayPixel = (pixel.b + pixel.g + pixel.r) / 3;
+			//Intensity grayPixel = (pixel.b*0.0722 + pixel.g*0.7152 + pixel.r*0.2126);
+			iImg->setPixel(i, grayPixel);			
+		}
+	return iImg;
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
