3b33a7b6736a3facd550301893500e10dbf3ba57
koen-github <koenvdkruk@gmail.com>
2016-04-25 22:16:37 +0200

    Added external pictures, changed algoritm

--- source/ExternalDLL/ExternalDLL/Main.cpp
+++ source/ExternalDLL/ExternalDLL/Main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char * argv[]) {
 
 
 	RGBImage * input = ImageFactory::newRGBImage();
-	if (!ImageIO::loadImage("C:\\Users\\koen\\Documents\\HU-Vision-Team-Koen\\testsets\\Set A\\TestSet Images\\male-3.png", *input)) {
+	if (!ImageIO::loadImage("C:\\Users\\koen\\Documents\\HU-Vision-Team-Koen\\testsets\\Set A\\TestSet Images\\female-3.png", *input)) {
 		std::cout << "Image could not be loaded!" << std::endl;
 		system("pause");
 		return 0;
--- source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
+++ source/ExternalDLL/ExternalDLL/StudentLocalization.cpp
@@ -1,7 +1,11 @@
 #include "StudentLocalization.h"
 #include <algorithm>
+#include "RGBImage.h"
+#include "ImageFactory.h"
+#include "HereBeDragons.h"
+#include "ImageIO.h"
 
-Feature StudentLocalization::headTop(const IntensityImage &image)const{
+Feature StudentLocalization::headTop(const IntensityImage &image) const{
 	unsigned int center = image.getWidth()/2;
 	const unsigned int default_y_step = 1;
 	const unsigned int default_x_step = 5;
@@ -58,27 +62,28 @@ Feature StudentLocalization::headTop(const IntensityImage &image)const{
 	return Feature{ Feature::FEATURE_HEAD_TOP, Point2D<double>(center, lowestValue) };
 }
 
-bool StudentLocalization::stepFindHead(const IntensityImage &image, FeatureMap &features) const {
+bool StudentLocalization::stepFindHead(const IntensityImage &imageIn, FeatureMap &features) const {
 
-	features.putFeature(headTop(image));
+	features.putFeature(headTop(imageIn));
 	
-	int width = image.getWidth();
-	int height = image.getHeight();
-	int possibleCenterXaxis = image.getWidth() / 2;
-	int possibleCenterYaxis = image.getHeight() / 2;
+	int width = imageIn.getWidth();
+	int height = imageIn.getHeight();
+	int centerXaxis = features.getFeature(Feature::FEATURE_HEAD_TOP).getX();
+	int possibleCenterYaxis = imageIn.getHeight() / 2;
 	int stepY = 1;
 	int stepX = 2;
+	int margeCenterLeftRight = 5; //in the middle of the image, some points are ignored 5 + 5 = 10.
 	std::map <int, int> numberOfPixelsPerXaxisHalfLeftSide{};
 	std::map <int, int> numberOfPixelsPerXaxisHalfRightSide{};
 
 	for (int dX = 0; dX < width; dX += stepX){
 		for (int dY = 0; dY < height; dY += stepY){
-			Intensity currentPixel = image.getPixel(dX, dY);
+			Intensity currentPixel = imageIn.getPixel(dX, dY);
 			if (currentPixel == 0){
-				if (dX <= possibleCenterXaxis){ //possible left side 
+				if (dX <= (centerXaxis - margeCenterLeftRight)){ //left side of head with marge
 					numberOfPixelsPerXaxisHalfLeftSide[dX] = numberOfPixelsPerXaxisHalfLeftSide[dX] + 1;
 				} 
-				else{ //possible right side
+				else if (dX >= (centerXaxis + margeCenterLeftRight)){ //right side of head with marge
 					numberOfPixelsPerXaxisHalfRightSide[dX] = numberOfPixelsPerXaxisHalfRightSide[dX] + 1;
 				}
 			}
@@ -93,12 +98,14 @@ bool StudentLocalization::stepFindHead(const IntensityImage &image, FeatureMap &
 		[](const std::pair<int, int>& p1, const std::pair<int, int>& p2) {
 		return p1.second < p2.second; })->first;
 
-	std::cout << "Left Side location x-axis: " << leftSideXaxis << "\n";
-	std::cout << "Right Side location x-axis: " << rightSideXaxis << "\n";
-	
-	features.putFeature(Feature(Feature::FEATURE_HEAD_LEFT_SIDE, Point2D<double>(leftSideXaxis, 142.0)));
-	features.putFeature(Feature(Feature::FEATURE_HEAD_TOP, Point2D<double>(95.0, 8.0)));
-	features.putFeature(Feature(Feature::FEATURE_HEAD_RIGHT_SIDE, Point2D<double>(rightSideXaxis, 142.0)));
+
+	features.putFeature(Feature(Feature::FEATURE_HEAD_LEFT_SIDE, Point2D<double>(leftSideXaxis, 116.0)));
+	features.putFeature(Feature(Feature::FEATURE_HEAD_RIGHT_SIDE, Point2D<double>(rightSideXaxis, 116.0)));
+
+	std::cout << "Left Side location x-axis:  (" << features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getX() << ", " << features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getY() << ")\n";
+	std::cout << "Right Side location x-axis:  (" << features.getFeature(Feature::FEATURE_HEAD_RIGHT_SIDE).getX() << ", " << features.getFeature(Feature::FEATURE_HEAD_RIGHT_SIDE).getY() << ")\n";
+	std::cout << "Top head location x-axis: (" << features.getFeature(Feature::FEATURE_HEAD_TOP).getX() << ", " << features.getFeature(Feature::FEATURE_HEAD_TOP).getY() << ")\n";
+
 		
 
 	//int highestValueLeftSide = numberOfPixelsPerXaxis
@@ -108,13 +115,20 @@ bool StudentLocalization::stepFindHead(const IntensityImage &image, FeatureMap &
 	//	std::cout << it->first << " --> " << it->second << "\n";
 	//}
 
+	IntensityImage * image = ImageFactory::newIntensityImage();
+	HereBeDragons::SonnetCLI(imageIn, *image);
 
-	//std::cout << "Pixel on 3,3 " << "\n" << int(kippetje) << "\n";
+	ImageIO::saveIntensityImage(*image, ImageIO::getDebugFileName("Localization-1/nose-removed.png"));
 
-	/*
-	cv::circle(outImage, cv::Point((int)headTop.getPoints()[0].x, (int)headTop.getPoints()[0].y), 1, cv::Scalar(0, 0, 255), -1, 8);
-	cv::circle(outImage, cv::Point((int)headLeftSide.getPoints()[0].x, (int)headLeftSide.getPoints()[0].y), 1, cv::Scalar(0, 0, 255), -1, 8);
-	cv::circle(outImage, cv::Point((int)headRightSide.getPoints()[0].x, (int)headRightSide.getPoints()[0].y), 1, cv::Scalar(0, 0, 255), -1, 8);
+	cv::Mat outImage;
+	HereBeDragons::HerLoveForWhoseDearLoveIRiseAndFall(*image, outImage);
+	cv::cvtColor(outImage, outImage, CV_GRAY2RGB);
+
+	
+	cv::circle(outImage, cv::Point((int)features.getFeature(Feature::FEATURE_HEAD_TOP).getX(), (int)features.getFeature(Feature::FEATURE_HEAD_TOP).getY()), 4, cv::Scalar(0, 0, 255), -1, 8);
+	cv::circle(outImage, cv::Point((int)features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getX(), (int)features.getFeature(Feature::FEATURE_HEAD_LEFT_SIDE).getY()), 4, cv::Scalar(0, 0, 255), -1, 8);
+	cv::circle(outImage, cv::Point((int)features.getFeature(Feature::FEATURE_HEAD_RIGHT_SIDE).getX(), (int)features.getFeature(Feature::FEATURE_HEAD_RIGHT_SIDE).getY()), 4, cv::Scalar(0, 0, 255), -1, 8);
+	
 
 	//Save debug image
 	RGBImage * outImageRGB = ImageFactory::newRGBImage();
@@ -123,11 +137,21 @@ bool StudentLocalization::stepFindHead(const IntensityImage &image, FeatureMap &
 	delete outImageRGB;
 	delete image;
 
+	//working images:
 
-	ImageIO::saveIntensityImage(*executor->resultPreProcessingStep4, ImageIO::getDebugFileName("Pre-processing-4.png"));
-	*/
-	//ImageIO::saveRGBImage(*outImageRGB, ImageIO::getDebugFileName("Localization-1/debug.png"));
+	//child-1.png
+	//female-1.png
+	//female-3.png == but NOT WORKING with Arno's version, we made it better :) I added an option for middle marge.
+	//male-3.png
+	//bouke-1.png == but NOT WORKING with Arno's version.
+	
 
+	//not working:
+	//female-2.png == but working with Arno's working
+	//male-1.png == but working with Arno's verion.
+	//male-2.png == but working with Arno's verion.
+	//koen-2.png == NOT WORKING
+	//arno-1.png == NOT WORKING.
 
 	return true;
 }
