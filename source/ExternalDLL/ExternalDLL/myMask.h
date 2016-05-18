3518226d7f1c0b5cfc44ed97ec9a1f9615081ce0
koen de gruijter <koen.degruijter@student.hu.nl>
2016-05-18 14:46:17 +0200

    no comment

--- source/ExternalDLL/ExternalDLL/myMask.h
+++ source/ExternalDLL/ExternalDLL/myMask.h
@@ -3,16 +3,19 @@
 #include <array>
 #include <iterator>
 #include <cmath>
+#include <iostream>
 
 struct mask{ //center is always 0,0
 private:
 	int offset;//zorgt er voor dat x negatief kan zijn om de array te benaderen
 	int width;// om de rij breedte te gebruiken om alles in een array te krijgen..
 	Intensity* msk; //dit is de mask array
+	int negOffset;
 
 public:
 	mask(unsigned int radius) : msk(new Intensity[ (int)pow(( 1 + radius * 2),2) ] ){ //1 = 1 pixel size mask\k & 2=9(3x3)pixelsize mask
 		offset = radius;
+		negOffset = offset *-1;
 		width = 1 + (radius * 2);
 	}
 	unsigned int getpixel(int x, int y){
@@ -25,11 +28,15 @@ public:
 
 
 	int getBegin(){//om begin van mask te pakken (voor zowel x als y)
-		return offset*-1;
+		return negOffset;
 	}
 
 	int getEnd(){ // (voor zowel x als y)
 		return offset;
+	}
+
+	int getWidth(){
+		return width;
 	}
 
 
@@ -46,13 +53,21 @@ public:
 	void laplacian(IntensityImage *img, mask& mask);
 
 	//calculeerd intensety
-	Intensity laplacianPixel(IntensityImage &img, unsigned int x, unsigned int y, mask &mask){
+	Intensity laplacianPixel(const IntensityImage &img, unsigned int x, unsigned int y,mask &mask){
 		int val = 0;
 		for (int x1 = mask.getBegin(); x1 < mask.getEnd(); x1++){
 			for (int y1 = mask.getBegin(); y1 < mask.getEnd(); y1++){
-
+				try{
+					val += img.getPixel(x + x1, y + y1)*mask.getpixel(x1,y1);
+				}
+				catch (std::exception ex){
+					std::cout << "pixel out of range, make propper catch code for this" << std::endl;
+				}
 			}
 		}
+		val = val / pow(mask.getWidth, 2);
+		if (val < 0){ return (Intensity)0; }
+		return (Intensity)255;
 	
 	}
 };
